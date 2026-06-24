18e62e0536ea5136c5c69d704c3973b2c10094a0
Heitor Cassimiro <heitorcroliveira@gmail.com>
2026-06-24 15:58:43 -0300

    Lista de listas

--- exercicio-tabuada.py
+++ exercicio-tabuada.py
@@ -2,11 +2,11 @@ entrada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 tabuada = int(input("Digite qual tabuada você quer visualizar: "))
 
-
 resultado = map(lambda x: x * tabuada, entrada)
 
 resultado = list(resultado)
 
+# Maneira não otimizada / Não recomendada
 print(
     f"A tabuada de {tabuada} é: {resultado[0]}, {resultado[1]}, {resultado[2]}, {resultado[3]}, {resultado[4]}, {resultado[5]},{resultado[6]} ,{resultado[7]} ,{resultado[8]}, {resultado[9]}"
 )
---
+++ lista-listas.py
@@ -0,0 +1,64 @@
+# Importando a biblioteca para gerar números aleatórios
+from random import randint
+
+# Lista de nomes e notas de alunos
+notas_turma = [
+    "João",
+    8.0,
+    9.0,
+    10.0,
+    "Maria",
+    9.0,
+    7.0,
+    6.0,
+    "José",
+    3.4,
+    7.0,
+    7.0,
+    "Cláudia",
+    5.5,
+    6.6,
+    8.0,
+    "Ana",
+    6.0,
+    10.0,
+    9.5,
+]
+
+# Efetuando a extração dos dados
+nomes = []
+notas_juntas = []
+
+for i in range(len(notas_turma)):
+
+    if i % 4 == 0:
+        nomes.append(notas_turma[i])
+    else:
+        notas_juntas.append(notas_turma[i])
+
+# Verificando informações de notas e nomes
+print(notas_juntas)
+print(nomes)
+
+# Extração dos dados da nota de cada aluno
+notas = []
+
+for i in range(0, len(notas_juntas), 3):
+    notas.append([notas_juntas[i], notas_juntas[i + 1], notas_juntas[i + 2]])
+
+print(notas)
+
+
+def gerar_codigo():
+    return str(randint(0, 999))
+
+
+codigo_estudantes = []
+
+
+for i in range(len(nomes)):
+
+    codigo_estudantes.append((nomes[i], nomes[i][0] + gerar_codigo()))
+
+
+print(codigo_estudantes)
