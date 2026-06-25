b7c74a8c306404ed96c8819ce45fe1d4dee7cdc5
Heitor Cassimiro <heitorcroliveira@gmail.com>
2026-06-25 08:21:46 -0300

    Lista compreensão e dicionários

---
+++ list-comprehension-if-else.py
@@ -0,0 +1,23 @@
+nomes = [
+    ("João", "J720"),
+    ("Maria", "M205"),
+    ("José", "J371"),
+    ("Cláudia", "C546"),
+    ("Ana", "A347"),
+]
+notas = [
+    [8.0, 9.0, 10.0],
+    [9.0, 7.0, 6.0],
+    [3.4, 7.0, 7.0],
+    [5.5, 6.6, 8.0],
+    [6.0, 10.0, 9.5],
+]
+medias = [9.0, 7.3, 5.8, 6.7, 8.5]
+
+
+situacao = ["Aprovado" if media >= 6 else "Reprovado" for media in medias]
+
+print(situacao)
+
+cadastro = [x for x in [nomes, notas, medias]]
+print(cadastro)
